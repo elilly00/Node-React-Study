a7db1d4ef95c83a6e4cfbe980c8fbc3afd4029e7
eliily00 <wjddms0700@gmail.com>
2022-07-18 21:05:23 +0900

    22.07.18: 로그아웃 기능 생성

--- middleware/auth.js
+++ middleware/auth.js
@@ -7,7 +7,7 @@ let auth = (req, res, next) => {
     let token = req.cookies.x_auth;
     
     // 2. token을 복호화 한 후 user를 찾음
-    User.findByToken(toekn, (err, user) => {
+    User.findByToken(token, (err, user) => {
         if(err) throw err;
         if(!user) return res.json({ isAuth: false, error: true});
 
